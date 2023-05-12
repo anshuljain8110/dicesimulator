934b1f29caa5741a6492a38dd690f9723755f5c0
anshuljain8110 <116940602+anshuljain8110@users.noreply.github.com>
2023-05-12 17:44:18 +0530

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let x=Math.floor(Math.random()*6)+1
+
+if(x==1){
+    document.getElementsByClassName("no5")[0].className="highlight";
+}
+else if(x==2){
+    document.getElementsByClassName("no1")[0].className="highlight";
+    document.getElementsByClassName("no9")[0].className="highlight";
+}
+else if(x==3){
+    document.getElementsByClassName("no1")[0].className="highlight";
+    document.getElementsByClassName("no9")[0].className="highlight";
+    document.getElementsByClassName("no5")[0].className="highlight";
+}
+else if(x==4){
+    document.getElementsByClassName("no1")[0].className="highlight";
+    document.getElementsByClassName("no3")[0].className="highlight";
+    document.getElementsByClassName("no7")[0].className="highlight";
+    document.getElementsByClassName("no9")[0].className="highlight";
+}
+else if(x==5){
+    document.getElementsByClassName("no1")[0].className="highlight";
+    document.getElementsByClassName("no3")[0].className="highlight";
+    document.getElementsByClassName("no7")[0].className="highlight";
+    document.getElementsByClassName("no9")[0].className="highlight";
+    document.getElementsByClassName("no5")[0].className="highlight";
+}
+else if(x==6){
+    document.getElementsByClassName("no1")[0].className="highlight";
+    document.getElementsByClassName("no3")[0].className="highlight";
+    document.getElementsByClassName("no7")[0].className="highlight";
+    document.getElementsByClassName("no9")[0].className="highlight";
+    document.getElementsByClassName("no4")[0].className="highlight";
+    document.getElementsByClassName("no6")[0].className="highlight";
+}
